fb6eb1d8eb288109f0922886e7cd4e537cef9ba6
CG Remuzzi <carlosremuzzi@gmail.com>
2020-04-05 01:59:08 -0400

    setting up new react example

---
+++ precache-manifest.37cd24c6e5a50826147bb0d12f711c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468139aac98733307f16d46f2d0741e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efec72711af71d8fe13b",
+    "url": "/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "4f9a8d83eb10526f401d",
+    "url": "/static/css/main.a9af0b1c.chunk.css"
+  },
+  {
+    "revision": "efec72711af71d8fe13b",
+    "url": "/static/js/2.82acb4aa.chunk.js"
+  },
+  {
+    "revision": "4f9a8d83eb10526f401d",
+    "url": "/static/js/main.96eaff6b.chunk.js"
+  },
+  {
+    "revision": "5a6dc6cb11cbeefcd2f1",
+    "url": "/static/js/runtime~main.b1da571b.js"
+  }
+]);
